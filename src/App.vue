5210aa6c454cd8e60e89925583f731dc0403ed8d
Ebru <yerdebru@gmail.com>
2025-10-23 22:16:50 +0300

    Initial commit: Vue 3 + Vuetify + Router + Pinia setup

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit
+    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a>
+    to read the documentation
+  </p>
+  <v-container>
+    <v-btn color="primary" border-sm>Hello Vuetify</v-btn>
+    <v-icon icon="mdi-domain" color="green-darken-2"></v-icon>
+  </v-container>
+</template>
+
+<style scoped></style>
